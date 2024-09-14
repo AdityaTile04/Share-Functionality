a31234a7907723ac229e940d40074753089bf47f
Aditya Tile <130766642+AdityaTile04@users.noreply.github.com>
2024-09-14 21:31:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import BlogPost from "./components/blogPost";
 
 function App() {
   const title = "My Awesome Blog Post";
-  const content = "This is the content of the blog post...";
+  const content = "This is the content of the blog post....";
   const url = "https://example.com/my-awesome-blog-post";
 
   return (
